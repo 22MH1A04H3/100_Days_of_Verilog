08aece475abd9fa15b988a8de8030179f31d72aa
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-04-07 13:07:48 +0530

    Create tb.v

---
+++ Day38/tb.v
@@ -0,0 +1,21 @@
+module tb;
+reg [4:0]in;
+wire [4:0]y;
+gtob uut(.in(in),.y(y));
+initial
+begin
+in=5'b10101;
+#5;
+in=5'b11100;
+#5;
+in=5'b10001;
+#5;
+in=5'b10111;
+#5;
+in=5'b11101;
+#5;
+in=5'b11111;
+#10;
+$finish();
+end
+endmodule
