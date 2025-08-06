968ccd3f4026f697a2cffedd87202d844b257e8a
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-08-06 19:51:38 +0530

    Create Transcript.v

---
+++ Day71/Transcript.v
@@ -0,0 +1,29 @@
+[2025-08-06 14:21:21 UTC] xrun -Q -unbuffered '-timescale' '1ns/1ns' '-sysv' '-access' '+rw' design.sv testbench.sv  
+TOOL:	xrun	23.09-s001: Started on Aug 06, 2025 at 10:21:21 EDT
+xrun: 23.09-s001: (c) Copyright 1995-2023 Cadence Design Systems, Inc.
+	Top level design units:
+		tb
+Loading snapshot worklib.tb:sv .................... Done
+xcelium> source /xcelium23.09/tools/xcelium/files/xmsimrc
+xcelium> run
+the square root values of 303379748 is = 17418
+the square root values of 2223298057 is = 47152
+the square root values of 112818957 is = 10622
+the cube root values of 2999092325 is = 1341
+the square root values of 15983361 is = 3998
+the square root values of 992211318 is = 31499
+the cube root values of 1993627629 is = 1172
+the cube root values of 2097015289 is = 1192
+the cube root values of 3807872197 is = 1451
+the square root values of 1924134885 is = 43865
+the square root values of 2301810194 is = 47977
+the cube root values of 2033215986 is = 1180
+the square root values of 4093672168 is = 63982
+the square root values of 777537884 is = 27884
+the square root values of 2527811629 is = 50277
+the cube root values of 2985255523 is = 1339
+Simulation complete via $finish(1) at time 170 NS + 0
+./testbench.sv:17       $finish();
+xcelium> exit
+TOOL:	xrun	23.09-s001: Exiting on Aug 06, 2025 at 10:21:23 EDT  (total: 00:00:02)
+Done
