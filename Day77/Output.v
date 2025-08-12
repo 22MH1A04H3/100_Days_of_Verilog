9a147d50c0d2d94f68a48f248931bb121ee2fb1d
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-08-12 20:06:09 +0530

    Create Output.v

---
+++ Day77/Output.v
@@ -0,0 +1,70 @@
+[2025-08-12 14:35:32 UTC] vlib work && vlog '-timescale' '1ns/1ns' design.sv testbench.sv  && vsim -c -do "vsim +access+r; run -all; exit"  
+VSIMSA: Configuration file changed: `/home/runner/library.cfg'
+ALIB: Library "work" attached.
+work = /home/runner/work/work.lib
+MESSAGE "Unit top modules: tb."
+SUCCESS "Compile success 0 Errors 0 Warnings  Analysis time: 0[s]."
+done
+# Aldec, Inc. Riviera-PRO version 2023.04.112.8911 built for Linux64 on May 12, 2023.
+# HDL, SystemC, and Assertions simulator, debugger, and design environment.
+# (c) 1999-2023 Aldec, Inc. All rights reserved.
+# ELBREAD: Elaboration process.
+# ELBREAD: Elaboration time 0.0 [s].
+# KERNEL: Main thread initiated.
+# KERNEL: Kernel process initialization phase.
+# ELAB2: Elaboration final pass...
+# ELAB2: Create instances ...
+# KERNEL: Time resolution set to 1ns.
+# ELAB2: Create instances complete.
+# SLP: Started
+# SLP: Elaboration phase ...
+# SLP: Elaboration phase ... done : 0.0 [s]
+# SLP: Generation phase ...
+# SLP: Generation phase ... done : 0.1 [s]
+# SLP: Finished : 0.1 [s]
+# SLP: 0 primitives and 3 (100.00%) other processes in SLP
+# SLP: 21 (95.45%) signals in SLP and 1 (4.55%) interface signals
+# ELAB2: Elaboration final pass complete - time: 0.1 [s].
+# KERNEL: SLP loading done - time: 0.0 [s].
+# KERNEL: Warning: You are using the Riviera-PRO EDU Edition. The performance of simulation is reduced.
+# KERNEL: Warning: Contact Aldec for available upgrade options - sales@aldec.com.
+# KERNEL: SLP simulation initialization done - time: 0.0 [s].
+# KERNEL: Kernel process initialization done.
+# Allocation: Simulator allocated 4674 kB (elbread=427 elab2=4111 kernel=135 sdf=0)
+# KERNEL: ASDB file was created in location /home/runner/dataset.asdb
+# KERNEL: w_en=1 | din= 36
+# KERNEL: w_en=1 | din=129
+# KERNEL: w_en=1 | din=  9
+# KERNEL: w_en=1 | din= 99
+# KERNEL: w_en=1 | din= 13
+# KERNEL: w_en=1 | din=141
+# KERNEL: w_en=1 | din=101
+# KERNEL: w_en=1 | din= 18
+# KERNEL: w_en=1 | din=  1
+# KERNEL: w_en=1 | din= 13
+# KERNEL: w_en=1 | din=118
+# KERNEL: w_en=1 | din= 61
+# KERNEL: w_en=1 | din=237
+# KERNEL: w_en=1 | din=140
+# KERNEL: w_en=1 | din=249
+# KERNEL: r_en=1 | dout=  0
+# KERNEL: r_en=1 | dout=  0
+# KERNEL: r_en=1 | dout= 36
+# KERNEL: r_en=1 | dout=129
+# KERNEL: r_en=1 | dout=  9
+# KERNEL: r_en=1 | dout= 99
+# KERNEL: r_en=1 | dout= 13
+# KERNEL: r_en=1 | dout=141
+# KERNEL: r_en=1 | dout=101
+# KERNEL: r_en=1 | dout= 18
+# KERNEL: r_en=1 | dout=  1
+# KERNEL: r_en=1 | dout= 13
+# KERNEL: r_en=1 | dout=118
+# KERNEL: r_en=1 | dout= 61
+# KERNEL: r_en=1 | dout=237
+# RUNTIME: Info: RUNTIME_0068 testbench.sv (43): $finish called.
+# KERNEL: Time: 420 ns,  Iteration: 0,  Instance: /tb,  Process: @INITIAL#15_1@.
+# KERNEL: stopped at time: 420 ns
+# VSIM: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
+Done
