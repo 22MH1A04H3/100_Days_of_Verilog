4f301ee089503f323e76ebc29dd3d5792e3dc123
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-08-24 12:04:26 +0530

    Create transcript.v

---
+++ Day86/transcript.v
@@ -0,0 +1,83 @@
+[2025-08-24 06:26:51 UTC] vlib work && vlog '-timescale' '1ns/1ns' design.sv testbench.sv  && vsim -c -do "vsim +access+r; run -all; exit"  
+VSIMSA: Configuration file changed: `/home/runner/library.cfg'
+ALIB: Library "work" attached.
+work = /home/runner/work/work.lib
+MESSAGE "Unit top modules: tb."
+SUCCESS "Compile success 0 Errors 0 Warnings  Analysis time: 0[s]."
+done
+# Aldec, Inc. Riviera-PRO version 2023.04.112.8911 built for Linux64 on May 12, 2023.
+# HDL, SystemC, and Assertions simulator, debugger, and design environment.
+# (c) 1999-2023 Aldec, Inc. All rights reserved.
+# ELBREAD: Elaboration process.
+# ELBREAD: Elaboration time 0.0 [s].
+# KERNEL: Main thread initiated.
+# KERNEL: Kernel process initialization phase.
+# ELAB2: Elaboration final pass...
+# KERNEL: PLI/VHPI kernel's engine initialization done.
+# PLI: Loading library '/usr/share/Riviera-PRO/bin/libsystf.so'
+# ELAB2: Create instances ...
+# KERNEL: Time resolution set to 1ns.
+# ELAB2: Create instances complete.
+# SLP: Started
+# SLP: Elaboration phase ...
+# SLP: Elaboration phase ... done : 0.0 [s]
+# SLP: Generation phase ...
+# SLP: Generation phase ... done : 0.1 [s]
+# SLP: Finished : 0.1 [s]
+# SLP: 0 primitives and 5 (100.00%) other processes in SLP
+# SLP: 9 (100.00%) signals in SLP and 0 interface signals
+# ELAB2: Elaboration final pass complete - time: 0.1 [s].
+# KERNEL: SLP loading done - time: 0.0 [s].
+# KERNEL: Warning: You are using the Riviera-PRO EDU Edition. The performance of simulation is reduced.
+# KERNEL: Warning: Contact Aldec for available upgrade options - sales@aldec.com.
+# KERNEL: SLP simulation initialization done - time: 0.0 [s].
+# KERNEL: Kernel process initialization done.
+# Allocation: Simulator allocated 4671 kB (elbread=427 elab2=4110 kernel=134 sdf=0)
+# KERNEL: ASDB file was created in location /home/runner/dataset.asdb
+# KERNEL: clk = 0 |reset = 1  | din = 0 | out = x
+# KERNEL: clk = 1 |reset = 1  | din = 0 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 0 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 1 | out = 1
+# KERNEL: clk = 0 |reset = 0  | din = 1 | out = 1
+# KERNEL: clk = 1 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 0 |reset = 0  | din = 1 | out = 0
+# KERNEL: clk = 1 |reset = 0  | din = 1 | out = 0
+# RUNTIME: Info: RUNTIME_0068 testbench.sv (63): $finish called.
+# KERNEL: Time: 220 ns,  Iteration: 0,  Instance: /tb,  Process: @INITIAL#12_2@.
+# KERNEL: stopped at time: 220 ns
