d3a9aadd15f89bb5b63bfc474b7a14d26aa97e06
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-08-25 22:52:33 +0530

    Create Sorting_descending_oreder.v

---
+++ Day88/Sorting_descending_oreder.v
@@ -0,0 +1,42 @@
+//desseding_order
+module sort(
+  input [31:0]a,b,c,d,e,f,
+  output reg [31:0]m,n,o,p,q,r);
+ 
+  //internal mem
+  reg [31:0]mem[5:0];
+  reg [31:0]t;
+  always@(*)
+    begin
+      mem[0]=a;
+      mem[1]=b;
+      mem[2]=c;
+      mem[3]=d;
+      mem[4]=e;
+      mem[5]=f;
+      for(int i=0;i<6;i++)
+        begin
+          for(int j=0;j<5;j++)
+            begin
+              if(mem[j]<mem[j+1])
+                begin
+                 // reg [31:0]t;
+                  t = mem[j];
+                  mem[j]=mem[j+1];
+                  mem[j+1]=t;
+                  
+                end
+              
+            end
+        end
+      
+      m=mem[0];
+      n=mem[1];
+      o=mem[2];
+      p=mem[3];
+      q=mem[4];
+      r=mem[5];
+      
+    end
+  
+endmodule
