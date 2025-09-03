08007ebd9d1b9381c2281254c40f5d6c607911e8
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-09-03 10:19:19 +0530

    Create Debounced_Circute.v

---
+++ Day94/Debounced_Circute.v
@@ -0,0 +1,28 @@
+//Debounce_circute
+module debounce(
+input clk,reset,
+input noise,
+output reg clean);
+  reg [3:0]shift_reg;
+  
+  always@(posedge clk)
+    begin
+      if(reset)
+        begin
+          clean<='b0;
+          shift_reg<='b0;
+          
+        end
+      else
+      begin
+        shift_reg<={shift_reg[2:0],noise};
+        if(shift_reg==4'b1111)
+          clean<=1;
+        else
+          clean<=0;
+        
+      end
+      
+      
+    end
+endmodule
